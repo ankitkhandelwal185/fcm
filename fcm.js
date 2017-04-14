bafa4810156bec5c3a8d034fb9d9eb7704d88b5c
iwayankit <ankitkhandelwal185@gmail.com>
2017-04-15 03:49:40 +0530

    updated nodejs code

--- fcm.js
+++ fcm.js
@@ -24,8 +24,17 @@ app.get('/api/fcm/get', function(request, response){
 })
 app.post('/api/fcm', function( request, response){
 
-//response.send(request.body.device_token);
+    //response.send(request.body.device_token);
     console.log("token "+request.query.device_token);
+    // Initialize the default app
+    /*var defaultApp = admin.initializeApp(defaultAppConfig);
+
+    console.log(defaultApp.name);  // "[DEFAULT]"*/
+
+    /*// Retrieve services via the defaultApp variable...
+    var defaultAuth = defaultApp.auth();
+    var defaultDatabase = defaultApp.database();*/
+
     // These registration tokens come from the client FCM SDKs.
     var registrationTokens = [
       /*"bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1...",
@@ -38,11 +47,11 @@ app.post('/api/fcm', function( request, response){
     // See the "Defining the message payload" section below for details
     // on how to define a message payload.
     var payload = {
-      data: {
-        score: "Need Help Please Help",
-        time: "2:45"
-      }
-    };
+  notification: {
+    title: "Help!!!!",
+    body: "Please, help me "
+  }
+};
 
     // Send a message to the devices corresponding to the provided
     // registration tokens.
